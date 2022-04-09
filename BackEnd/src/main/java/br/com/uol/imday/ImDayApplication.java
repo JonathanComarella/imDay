1540bd4a18334e21b106d4d4de81349b0cf4813b
Jonathan Comarella <jonathanwcomarella@gmail.com>
2022-04-09 14:29:22 -0300

    Project Created

---
+++ BackEnd/src/main/java/br/com/uol/imday/ImDayApplication.java
@@ -0,0 +1,13 @@
+package br.com.uol.imday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImDayApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/br/com/uol/imday/ImDayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol.imday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

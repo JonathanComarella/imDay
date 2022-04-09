0dceaff8fb8f5066320a51bbaf38d2fc12f29f29
Jonathan Comarella <jonathanwcomarella@gmail.com>
2022-04-09 15:23:07 -0300

    Config Swagger

--- BackEnd/src/main/java/br/com/uol/imday/controller/ImDayController.java
+++ BackEnd/src/main/java/br/com/uol/imday/controller/ImDayController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,10 @@ public class ImDayController {
     private ResponseEntity<List<SolversDaysDTO>> listSolversDays() {
         List<UserDTO> usersList = userService.findAll();
         return ResponseEntity.ok().body(userService.solversDayList(usersList));
+    }
+
+    @GetMapping("/docs")
+    public void docs(HttpServletResponse response) throws IOException {
+        response.sendRedirect("http://localhost:8080/swagger-ui/index.html");
     }
 }
